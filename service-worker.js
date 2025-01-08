3f2ef8100e31e87b0f52523821d8baf2d94588a0
GBPRODUCE <145754423+GBPRODUCE@users.noreply.github.com>
2025-01-08 16:06:00 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,30 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker 安裝中...');
+// service-worker.js
+const CACHE_NAME = 'barcode-cache';
+const urlsToCache = [
+    '/barcode/index.html',
+    '/barcode/manifest.json',
+    '/barcode/GBP.LOGO.png',
+    '/barcode/category.js',
+    '/barcode/styless.css',
+];
+
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('barcode-cache').then((cache) => {
-            return cache.addAll([
-                '/barcode/index.html',
-                '/barcode/manifest.json',
-                '/barcode/GBP.LOGO.png',
-                '/barcode/category.js',
-                '/barcode/styless.css',
-            ]);
-        })
+        caches.open(barcode-cache)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 });
