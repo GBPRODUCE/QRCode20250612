8047431772a3e413aa6f04dc8aeec88eba778f11
GBPRODUCE <145754423+GBPRODUCE@users.noreply.github.com>
2025-01-08 16:58:29 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(barcode-cache)
+        caches.open(CACHE_NAME)
             .then(cache => {
                 return cache.addAll(urlsToCache);
             })
