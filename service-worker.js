7a71d1095b3f24f3808ae4ef54adc59e63c3f7d0
GBPRODUCE <145754423+GBPRODUCE@users.noreply.github.com>
2025-01-10 10:11:05 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,17 +8,17 @@ const urlsToCache = [
     '/barcode/styles.css',
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
+        caches.open(CACHE_NAME).then(function(cache) {
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
     );
